18cd0180e07be4c84d6acd4aa5a9fdbeee178108
Mehul <mehul.aryan.2222@gmail.com>
2025-11-29 16:28:40 +0530

    added env and gitignore

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -18,6 +21,14 @@ func main() {
 	
 	app := fiber.New()
 	
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
 	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(todos)
 	})
@@ -62,5 +73,5 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error" : "Todo not found"})
 	})
 
-	log.Fatal(app.Listen(":4002"))
+	log.Fatal(app.Listen(":"+PORT))
 }
