d604973fdcd0a6a15e008e9c6ecb6a6e3c0c930c
Mehul <mehul.aryan.2222@gmail.com>
2025-12-01 20:50:42 +0530

    added UI changes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -15,7 +16,7 @@ import (
 )
 
 type Todo struct {
-	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
@@ -24,10 +25,12 @@ var collection *mongo.Collection
 
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading .env fil:",err)
+	if os.Getenv("ENV") != "production" {
+		// Load the .env file if not in production 
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env fil:",err)
+		}
 	}
 
 	MONGODB_URI := os.Getenv("MONGODB_URI")
@@ -51,7 +54,11 @@ func main() {
 	
 	app := fiber.New()
 	
-
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin, Content-Type, Accept",
+	// }))
+	
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
@@ -61,6 +68,10 @@ func main() {
 
 	if port == "" {
 		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/","./client/dist")
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
